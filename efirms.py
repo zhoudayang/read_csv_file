e9f8a91c464f7a91bfdefee28361aa96859bcec8
周炀 <zhoudayang2@163.com>
2016-07-06 23:46:02 +0800

    delete before insert

--- efirms.py
+++ efirms.py
@@ -4,5 +4,13 @@ import pandas as pd
 import MySQLdb
 
 con = MySQLdb.connect(host="127.0.0.1",port=3306,user="root",db="ezlife",charset ="utf8")
+
+# delete before insert
+cur = con.cursor()
+delete_sql = "delete from efirms"
+cur.execute(delete_sql)
+con.commit()
+cur.close()
+
 df = pd.read_csv("/Users/zhouyang/Desktop/efirms.csv")
 pd.io.sql.to_sql(df, 'efirms', con, flavor='mysql', if_exists='append', index=False)
