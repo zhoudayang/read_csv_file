36af455681c52cf2ca5fafb569341c1a1ba89d37
周炀 <zhoudayang2@163.com>
2016-07-08 19:14:07 +0800

    Update test_result.py

--- test_result.py
+++ test_result.py
@@ -99,10 +99,12 @@ df['date'] = df['date'].map(lambda x: x if pd.isnull(x) else transform_date(x))
 
 try:
     pd.io.sql.to_sql(df, 'test_result', con, flavor='mysql', if_exists='append', index=False)
-except:
+except Exception,e:
+    print e
     print 'there is an error, please fix it before continue!'
     exit(-1)
-    # transfer data to remote mysql server
+
+# transfer data to remote mysql server
 yihuo_con = MySQLdb.connect(host="52.192.115.115", user="root", passwd="yihuo_root", port=3306, charset="utf8",
                             db="ezlife")
 rebuild_table(table_name="test_result", con=yihuo_con, df=df)
