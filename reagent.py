d7bafd0b34c860d91317b64efe80941c31ee84aa
zhoudayang <zhoudayang2@163.com>
2016-07-18 22:03:54 +0800

    change code to meet changes

--- reagent.py
+++ reagent.py
@@ -1,17 +1,18 @@
-# -*- coding:utf-8 -*- 
+# -*- coding:utf-8 -*-
 # author：zhouyang
 import pandas as pd
 import MySQLdb
 from datetime import datetime
 import numpy as np
-from util import delete_table,rebuild_table
+from util import delete_table, rebuild_table
+import re
 
 con = MySQLdb.connect(host="127.0.0.1", port=3306, user="root", db="ezlife", charset="utf8")
 
 # 删除原来表的内容
-delete_table("reagent",con)
+delete_table("reagent", con)
 
-df = pd.read_csv("/Users/zhouyang/Downloads/20160706/reagent.csv")
+df = pd.read_csv("/Users/zhouyang/Downloads/20160718/reagent.csv")
 
 # 需要更换列名的列,及更换之后的列名对应关系
 rename_dict = {
@@ -20,31 +21,14 @@ rename_dict = {
     "created_data": "create_date"
 }
 df = df.rename(columns=rename_dict)
-df['purchase_date'] = df['purchase_date'].map(lambda x: x if pd.isnull(x) else str(int(x)))
-df['arrival_date'] = df['arrival_date'].map(lambda x: x if pd.isnull(x) else str(int(x)))
-df['create_date'] = df['create_date'].map(lambda x: x if pd.isnull(x) else str(int(x)))
-
 
 # 将日期字符串转换为datetime数据类型
-def transform_date(x):
-    # 对不规范日期,记为空
-    if len(x) != 14:
-        print 'date is wrong. please check it later!'
-        print 'the wrong date is ', x
+def transform_date(date_str):
+    if re.match("\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}", date_str) is None:
+        print 'date string is not right please check it later!'
         return np.nan
-    year = int(x[:4])
-    month = int(x[4:6])
-    # 月份出现了0,非法!
-    if month <= 0:
-        month = 1
-    day = int(x[6:8])
-    # 日期出现了0,非法!
-    if day <= 0:
-        day = 1
-    hour = int(x[8:10])
-    min = int(x[10:12])
-    second = int(x[12:])
-    return datetime(year=year, month=month, day=day, hour=hour, minute=min, second=second)
+    return datetime.strptime(date_str, "%Y-%m-%d-%H-%M-%S")
+
 
 
 df['purchase_date'] = df['purchase_date'].map(lambda x: x if pd.isnull(x) else transform_date(x))
@@ -52,11 +36,14 @@ df['arrival_date'] = df['arrival_date'].map(lambda x: x if pd.isnull(x) else tra
 df['create_date'] = df['create_date'].map(lambda x: x if pd.isnull(x) else transform_date(x))
 try:
     pd.io.sql.to_sql(df, 'reagent', con, flavor='mysql', if_exists='append', index=False)
-except Exception,e:
+except Exception, e:
     print e
     print 'there is an error, please fix it before continue!'
     exit(-1)
-    
+
+# 此处表结构有变化,需要使用navicat进行数据同步
+
+
 # transfer data to remote mysql server
 yihuo_con = MySQLdb.connect(host="52.192.115.115", user="root", passwd="yihuo_root", port=3306, charset="utf8",
                             db="ezlife")
